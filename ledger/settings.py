0db685af3854382dc2bf6c279a57cd10c57f92cf
n1lby73 <n1lby73@gmail.com>
2026-01-01 11:06:36 +0100

    added rest framework and vendor app

--- ledger/settings.py
+++ ledger/settings.py
@@ -41,6 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+
+    'rest_framework',
+
+    # Manually added apps
+
+    'vendors',
 ]
 
 MIDDLEWARE = [
