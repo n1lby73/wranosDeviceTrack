847cadcad22f05793d611c7df80bec9d2ed9de26
n1lby73 <n1lby73@gmail.com>
2026-01-01 11:07:13 +0100

    serializers for login and registration

---
+++ vendors/serializers.py
@@ -0,0 +1,81 @@
+from .models import vendorModel
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+
+class vendorRegSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+
+        model = vendorModel
+        fields = ('address', 'email', 'password', 'businessName', 'phoneNumber', 'RCNumber', 'verifiedAddress', 'verifiedEmail')
+
+        def validateEmail(self, value):
+
+            if User.objects.filter(email=value.lower()).exists():
+
+                raise serializers.ValidationError("Email already exists.")
+
+            return value.lower()
+
+        def validateBusinessName(self, value):
+
+            if User.objects.filter(businessName=value.lower()).exists():
+
+                raise serializers.ValidationError("Business name already exists.")
+
+            return value.lower()
+
+        def validatePhoneNumber(self, value):
+
+            if User.objects.filter(phoneNumber=value).exists():
+
+                raise serializers.ValidationError("Phone number already exists.")
+
+            return value
+
+        def validateRCNumber(self, value):
+
+            if vendorModel.objects.filter(RCNumber=value).exists():
+
+                raise serializers.ValidationError("Registration number already exists.")
+
+            return value
+
+        
+        def create(self, validatedData):
+
+            vendor = vendorModel(
+                address=validatedData['address'],
+                email=validatedData['email'],
+                businessName=validatedData['businessName'],
+                phoneNumber=validatedData['phoneNumber'],
+                RCNumber=validatedData['RCNumber'],
+                verifiedAddress=validatedData['verifiedAddress'],
+                verifiedEmail=validatedData['verifiedEmail']
+            )
+
+            vendor.set_password(validatedData['password'])
+            vendor.save()
+            return vendor
+
+class VendorLoginSerializer(serializers.Serializer):
+
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        # Authenticate the user
+        vendor = authenticate(email=email, password=password)
+
+        # Check if vendor exists
+        if vendor is None:
+            raise serializers.ValidationError("Invalid email or password.")
+
+        attrs['vendor'] = vendor  # Add the authenticated vendor to the validated attributes
+        return attrs
